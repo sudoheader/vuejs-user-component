3820ae07011c4aad85d67c3ffc97d1ebfe7c7662
Raymond Nazaryan <rayn2427@gmail.com>
2018-03-11 19:33:22 -0700

    Set up parent-child communication with props

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,14 +2,14 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <!-- <p>User Name: {{ name }}</p> -->
+        <p>User Name: {{ name }}</p>
     </div>
 </template>
 
 <script>
-    // export default {
-
-    // }
+    export default {
+        props: ['name']
+    }
 </script>
 
 <style scoped>
