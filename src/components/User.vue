10e6e12b7359aafe6f18fb055d9389c2be8d1fe9
Raymond Nazaryan <rayn2427@gmail.com>
2018-03-15 20:18:34 -0700

    Added callback function resetFn

--- src/components/User.vue
+++ src/components/User.vue
@@ -7,7 +7,10 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail 
+                    :myName="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -29,6 +32,9 @@
         methods: {
             changeName() {
                 this.name = 'Alice';
+            },
+            resetName() {
+                this.name = 'Ray';
             }
         },
         components: {
