51e425acc3aa7f2c8658609b12d747870de08786
Raymond Nazaryan <rayn2427@gmail.com>
2018-03-16 17:20:44 -0700

    Added communication between sibling components

--- src/components/User.vue
+++ src/components/User.vue
@@ -10,10 +10,15 @@
                 <app-user-detail 
                     :myName="name" 
                     @nameWasReset="name = $event"
-                    :resetFn="resetName"></app-user-detail>
+                    :resetFn="resetName"
+                    :userAge="age">
+                </app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEdited="age = $event">
+                </app-user-edit>
             </div>
         </div>
     </div>
@@ -26,7 +31,8 @@
     export default {
         data: function() {
             return {
-                name: 'Ray'
+                name: 'Ray',
+                age: 26
             };
         },
         methods: {
